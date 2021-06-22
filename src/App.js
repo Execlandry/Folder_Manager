cdf53f46d8d248eee6c896b0bd72f984a5126e57
Execlandry <nikhilkerkarnk21@gmail.com>
2021-06-22 10:00:46 +0530

    made some changes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 
 function App() {
   return (
     <div className="App">
       <Folder name="Desktop" >
-        <Folder name = "Music">
-          <File name = "yoyo.mp4"/>
-          <File name = "just.mp4"/>
+
+        <Folder name="Music">
+          <File name="yoyo.mp4" />
+          <File name="just.mp4" />
         </Folder>
-        <File name="dog.png" />
-        <File name="cat.png" />
+
+        <Folder name = "Images">
+           <File name="dog.png" />
+           <File name="cat.png" />
+        </Folder>
+
       </Folder>
 
       <Folder name="Applications" />
@@ -18,40 +23,37 @@ function App() {
 }
 const Folder = (props) => {
   const [isOpen, setIsOpen] = useState(true);
-  const {name,children}=props;
-  const direction = isOpen?'down':'right'
+  const { name, children } = props;
+  const direction = isOpen ? 'down' : 'right'
 
-  const handleClick = ()=>setIsOpen(!isOpen)
-
-  // console.log(props);
+  const handleClick = () => setIsOpen(!isOpen)
   const borderStyle = { marginLeft: '17px' }
   return (
     <div >
       <span onClick={handleClick}>
-      <i className="blue folder icon"></i>
-      <i className={`caret ${direction} icon`}></i>
-
+        <i className="blue folder icon"></i>
+        <i className={`caret ${direction} icon`}></i>
       </span>
       {name}
+      
       <div style={borderStyle}>
-        {isOpen?children :null}
+        {isOpen ? children : null}
       </div>
     </div>
   )
 }
 
 const File = (props) => {
-  const {name}=props;
+  const { name } = props;
   const fileExtension = name.split('.')[1];
   const fileIcons = {
-    mp4 : 'headphones',
-    png : 'file image',
-    jpeg : 'file image outline'
+    mp4: 'headphones',
+    png: 'file image',
+    jpeg: 'file image outline'
   }
   return (
     <div>
       <i className={`${fileIcons[fileExtension]} icon`}></i>
-
       {name}
     </div>
   )
